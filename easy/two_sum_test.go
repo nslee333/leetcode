f981c7d20f0b17d1af85697b27a05f5de09e8ca9
Nathan <nslee333@gmail.com>
2023-06-24 17:06:31 -0700

    Test cases setup for two sum

--- easy/two_sum_test.go
+++ easy/two_sum_test.go
@@ -1,14 +1,38 @@
-package main
+package two_sum
 
 import (
 	"reflect"
 	"testing"
 )
 
-func TwoSumTableTest(t *testing.T) {
+func TestTwoSum(t *testing.T) {
 	t.Run("checks for correct output with example 1 input", func(t *testing.T) {
 		nums := []int{2, 7, 11, 15}
 		target := 9
+
+		got := twoSum(nums, target)
+		want := []int{0, 1}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("checks for correct output with example 1 input", func(t *testing.T) {
+		nums := []int{3, 2, 4}
+		target := 6
+
+		got := twoSum(nums, target)
+		want := []int{1, 2}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("checks for correct output with example 1 input", func(t *testing.T) {
+		nums := []int{3, 3}
+		target := 6
 
 		got := twoSum(nums, target)
 		want := []int{0, 1}
