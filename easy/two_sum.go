5e046033112779a01e0b701071819c2a43c4bd59
Nathan <nslee333@gmail.com>
2023-06-24 17:14:15 -0700

    working twoSum function.

--- easy/two_sum.go
+++ easy/two_sum.go
@@ -1,16 +1,20 @@
 package two_sum
 
-// Prompt:
+/*
 
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Prompt:
 
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-// You can return the answer in any order.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+*/
 
 func twoSum(nums []int, target int) []int {
 	result := make([]int, 0)
@@ -21,7 +25,7 @@ func twoSum(nums []int, target int) []int {
 		num2 := nums[i+1]
 
 		if num+num2 == target {
-			return append(result, num, num2)
+			return append(result, i, i+1)
 		} else {
 			continue
 		}
