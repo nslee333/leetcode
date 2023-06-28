a44549bb416035f436a365899687150acdd7dc3e
Nathan <nslee333@gmail.com>
2023-06-27 20:37:09 -0700

    Setup test and barebones of isValid func

--- easy/valid_parentheses.go
+++ easy/valid_parentheses.go
@@ -1,1 +1,19 @@
 package easy
+
+/*
+
+	Govem a string s containing just the characters () {} and []
+	determine if the input is valid.
+
+	 input is valid if:
+	- Open brackets must be closed by the same type of brackets.
+	- Open brackets must be closed in the correct order.
+	- Every close bracket has a corresponding open bracket of the same type.
+
+
+
+*/
+
+func isValid(s string) bool {
+	return false
+}
