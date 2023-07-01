8d95562068caa54e5d2f74d64f9a918bc3508186
Nathan <nslee333@gmail.com>
2023-07-01 13:53:30 -0700

    palindrome init

---
+++ easy/palindrome/palindrome.go
@@ -0,0 +1,18 @@
+package easy
+
+/*
+
+Given an integer `x`, return true if x is a palindrome, and false if otherwise.
+
+Ex 1:
+	Input: 121
+	Output: True
+	Explanation: 121 reads as 121 from left to right and from right to left.
+
+
+
+*/
+
+func isPalindrome(x int) bool {
+	return false
+}
---
+++ easy/palindrome/palindrome_test.go
@@ -0,0 +1,25 @@
+package easy
+
+import "testing"
+
+func TestPalindrome(t *testing.T) {
+	var cases = []struct {
+		name     string
+		input    int
+		expected bool
+	}{
+		{name: "ex. 1", input: 121, expected: true},
+		{name: "ex. 2", input: -121, expected: false},
+		{name: "ex. 3", input: 10, expected: false},
+		{name: "ex. 4", input: 1331, expected: true},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := isPalindrome(c.input)
+			if got != c.expected {
+				t.Errorf("got %v, expected %v", got, c.expected)
+			}
+		})
+	}
+}
