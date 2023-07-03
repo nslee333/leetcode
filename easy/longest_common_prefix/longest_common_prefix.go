77df330c44c7768e70c5759d43fe6bedc13872a7
Nathan <nslee333@gmail.com>
2023-07-03 15:18:03 -0700

    Progress save

--- easy/longest_common_prefix/longest_common_prefix.go
+++ easy/longest_common_prefix/longest_common_prefix.go
@@ -1,1 +1,35 @@
 package easy
+
+import "strings"
+
+/*
+
+Write a function to find the longest common prefix string amongst an array of strings
+
+If no common prefix, return an empty string
+
+
+*/
+
+func longestCommonPrefix(input []string) string {
+	result := ""
+
+	for i := range input {
+		pos := ""
+
+		if i < len(input)-1 {
+			first := strings.Split(input[i], "")
+			second := strings.Split(input[i+1], "")
+
+			if first[i] == second[i] {
+
+			} else {
+				break
+			}
+
+		}
+
+	}
+
+	return result
+}
