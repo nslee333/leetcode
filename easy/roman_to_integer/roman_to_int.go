607f0e701cdde7894df852d779dd0dc53067fb21
Nathan <nslee333@gmail.com>
2023-07-03 12:05:42 -0700

    working leetcode solution

--- easy/roman_to_integer/roman_to_int.go
+++ easy/roman_to_integer/roman_to_int.go
@@ -1,5 +1,9 @@
 package easy
 
+import (
+	"strings"
+)
+
 /*
 
 	Given a roman numeral, convert it into an integer.
@@ -7,5 +11,28 @@ package easy
 */
 
 func romanToInt(input string) int {
-	return 0
+	m := make(map[string]int)
+
+	m["I"] = 1
+	m["V"] = 5
+	m["X"] = 10
+	m["L"] = 50
+	m["C"] = 100
+	m["D"] = 500
+	m["M"] = 1000
+
+	slice := strings.Split(input, "")
+	result := 0
+
+	for i := 0; i < len(slice); i++ {
+
+		if i < len(slice)-1 && m[slice[i]] < m[slice[i+1]] {
+			result -= m[slice[i]]
+
+		} else {
+			result += m[slice[i]]
+		}
+	}
+
+	return result
 }
