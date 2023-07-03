93e07f673089957cf92a3a0194eaafcaec2e721b
Nathan <nslee333@gmail.com>
2023-07-03 10:18:40 -0700

    roman_to_int init

---
+++ easy/roman_to_integer/roman_to_int.go
@@ -0,0 +1,11 @@
+package easy
+
+/*
+
+	Given a roman numeral, convert it into an integer.
+
+*/
+
+func romanToInt(input string) int {
+	return 0
+}
---
+++ easy/roman_to_integer/roman_to_int_test.go
@@ -0,0 +1,27 @@
+package easy
+
+import "testing"
+
+func TestRomanToInt(t *testing.T) {
+	cases := []struct {
+		testName string
+		input    string
+		output   int
+	}{
+		{testName: "Ex.1", input: "III", output: 3},
+		{testName: "Ex.2", input: "LVIII", output: 58},
+		{testName: "Ex.3", input: "MCMXCIV", output: 1994},
+	}
+
+	for _, c := range cases {
+		t.Run(c.testName, func(*testing.T) {
+			got := romanToInt(c.input)
+			want := c.output
+
+			if got != want {
+				t.Errorf("got %v, want %v, input %v", got, want, c.input)
+			}
+		})
+	}
+
+}
