ee7019cbdc312eb89e3c7f66b83dbdbfe3fb73f9
Nathan <nslee333@gmail.com>
2023-07-03 13:31:43 -0700

    Test setup

--- easy/longest_common_prefix/longest_common_prefix_test.go
+++ easy/longest_common_prefix/longest_common_prefix_test.go
@@ -1,1 +1,26 @@
 package easy
+
+import "testing"
+
+func TestLongestCommonPrefix(t *testing.T) {
+
+	cases := []struct {
+		testName string
+		input    []string
+		output   string
+	}{
+		{testName: "Ex.1", input: []string{"flower", "flow", "flight"}, output: "fl"},
+		{testName: "Ex.2", input: []string{"flower", "flow", "flight"}, output: ""},
+	}
+
+	for _, c := range cases {
+		t.Run(c.testName, func(t *testing.T) {
+			got := longestCommonPrefix(c.input)
+			want := c.output
+
+			if got != want {
+				t.Errorf("got `%v`, want %v, input %v", got, want, c.input)
+			}
+		})
+	}
+}
