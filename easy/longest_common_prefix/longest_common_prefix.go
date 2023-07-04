1e39994c70f33fd503ec1720694443321a03df3d
Nathan <nslee333@gmail.com>
2023-07-04 10:41:35 -0700

    Working solution

--- easy/longest_common_prefix/longest_common_prefix.go
+++ easy/longest_common_prefix/longest_common_prefix.go
@@ -1,9 +1,6 @@
 package easy
 
-import (
-	"fmt"
-	"strings"
-)
+import "sort"
 
 /*
 
@@ -17,39 +14,18 @@ If no common prefix, return an empty string
 // &
 
 func longestCommonPrefix(input []string) string {
-	result := ""
-
-	for i := range input {
-		temp := make([]string, 0)
-
-		if i < len(input)-1 {
-			first := strings.Split(input[i], "")
-			second := strings.Split(input[i+1], "")
-
-			if 0 < len(first)-1 && 0 < len(second)-1 && first[0] == second[0] {
-				for j := range first {
-
-					if j < len(input)-1 && first[j] == second[j] {
-						temp = append(temp, first[j])
-						continue
-
-					} else {
-						break
-					}
-
-				}
-
-			}
-
-			result = strings.Join(temp, "")
-		} else if len(input) == 1 {
-			result = input[i]
-		} else if i < len(input)-1 && input[i] != input[i+1] {
-			result = ""
-		}
-		return result
+	if len(input) == 1 {
+		return input[0]
 	}
 
-	fmt.Println(result)
-	return result
+	sort.Strings(input)
+
+	l := len(input)
+	for i := range input[0] {
+		if input[0][i] != input[l-1][i] {
+			return input[0][:i]
+		}
+	}
+
+	return input[0]
 }
