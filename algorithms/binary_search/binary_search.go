b727f9ea0ea483ef90a0816b1ebe3c5d876d5fcf
Nathan <nslee333@gmail.com>
2023-07-06 13:50:10 -0700

    Working binary_search algo

--- algorithms/binary_search/binary_search.go
+++ algorithms/binary_search/binary_search.go
@@ -1,11 +1,35 @@
 package algorithms
 
+import (
+	"errors"
+)
+
 /*
 
 binary search written in go
 
 */
 
-func binary_search(inputArr []string, target string) int {
-	return 0
+func binary_search(inputArr []string, target string) (int, error) {
+	low := 0
+	high := len(inputArr) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := inputArr[mid]
+
+		if guess == target {
+			return mid, nil
+
+		} else if guess > target {
+
+			high = mid - 1
+			continue
+		} else {
+
+			low = mid + 1
+			continue
+		}
+	}
+	return -1, errors.New("target is not in array")
 }
