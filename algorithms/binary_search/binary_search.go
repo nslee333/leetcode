a8cf677c9bdd665faeb9290ceeae31b3481ce61d
Nathan <nslee333@gmail.com>
2023-07-06 13:25:51 -0700

    Filled in test and search frame

--- algorithms/binary_search/binary_search.go
+++ algorithms/binary_search/binary_search.go
@@ -1,1 +1,11 @@
 package algorithms
+
+/*
+
+binary search written in go
+
+*/
+
+func binary_search(inputArr []string, target string) int {
+	return 0
+}
--- algorithms/binary_search/binary_search_test.go
+++ algorithms/binary_search/binary_search_test.go
@@ -1,1 +1,26 @@
 package algorithms
+
+import "testing"
+
+func TestBinarySearch(t *testing.T) {
+	cases := []struct {
+		testName string
+		inputArr []string
+		target   string
+		expected int
+	}{
+		{testName: "Ex.1", inputArr: []string{"a", "b", "c", "d", "e", "f"}, target: "b", expected: 1},
+		{testName: "Ex.2", inputArr: []string{"a", "b", "c", "d", "e", "f"}, target: "d", expected: 3},
+		{testName: "Ex.3", inputArr: []string{"a", "b", "c", "d", "e", "f"}, target: "f", expected: 5},
+	}
+
+	for _, c := range cases {
+		t.Run(c.testName, func(t *testing.T) {
+			got := binarySearch(c.inputArr, c.target)
+
+			if got != c.expected {
+				t.Errorf("Got %v, expected %v, inputArr %v, target %v", got, c.expected, c.target)
+			}
+		})
+	}
+}
