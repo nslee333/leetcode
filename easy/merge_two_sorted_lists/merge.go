d42721c685acf4146fa51ea09e977120dea84253
Nathan <nslee333@gmail.com>
2023-07-06 12:34:11 -0700

    WIP

--- easy/merge_two_sorted_lists/merge.go
+++ easy/merge_two_sorted_lists/merge.go
@@ -1,5 +1,26 @@
 package easy
 
+/*
+
+Merge two lists in a one sorted list.
+
+The list should be made by splicing together the nodes of the first two
+lists.
+
+
+
+
+*/
+
 func Merge(input1, input2 []int) []int {
-	return make([]int, 0)
+	result := make([]int, 0)
+	for i := range input1 {
+		result = append(result, input1[i])
+	}
+
+	for i := range input2 {
+		result = append(result, input2[i])
+	}
+
+	sort
 }
