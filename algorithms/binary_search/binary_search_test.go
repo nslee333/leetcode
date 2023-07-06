5bef1e6dc9b82d69b56a24efefb9a5107cb4173f
Nathan <nslee333@gmail.com>
2023-07-06 13:50:08 -0700

    Added error handling

--- algorithms/binary_search/binary_search_test.go
+++ algorithms/binary_search/binary_search_test.go
@@ -1,6 +1,9 @@
 package algorithms
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestBinarySearch(t *testing.T) {
 	cases := []struct {
@@ -12,11 +15,16 @@ func TestBinarySearch(t *testing.T) {
 		{testName: "Ex.1", inputArr: []string{"a", "b", "c", "d", "e", "f"}, target: "b", expected: 1},
 		{testName: "Ex.2", inputArr: []string{"a", "b", "c", "d", "e", "f"}, target: "d", expected: 3},
 		{testName: "Ex.3", inputArr: []string{"a", "b", "c", "d", "e", "f"}, target: "f", expected: 5},
+		{testName: "Ex.4", inputArr: []string{"a", "b", "c", "d", "e", "f"}, target: "z", expected: -1},
 	}
 
 	for _, c := range cases {
 		t.Run(c.testName, func(t *testing.T) {
-			got := binary_search(c.inputArr, c.target)
+			got, error := binary_search(c.inputArr, c.target)
+
+			if error != nil {
+				fmt.Println(c.testName, error)
+			}
 
 			if got != c.expected {
 				t.Errorf("Got %v, expected %v, inputArr %v, target %v", got, c.expected, c.inputArr, c.target)
