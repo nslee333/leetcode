f00d4f723ba1489ea18ab8399cab1d0021402a54
Nathan <nslee333@gmail.com>
2023-07-06 18:11:38 -0700

    Working solution and helpers

--- easy/merge_two_sorted_lists/merge.go
+++ easy/merge_two_sorted_lists/merge.go
@@ -12,14 +12,62 @@ lists.
 
 */
 
-func Merge(input1, input2 []int) []int {
-	result := make([]int, 0)
-	for i := range input1 {
-		result = append(result, input1[i])
+// & Helper functions / types
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func createLinkedList(arr []int) *ListNode {
+	if len(arr) == 0 {
+		return nil
 	}
 
-	for i := range input2 {
-		result = append(result, input2[i])
+	head := &ListNode{Val: arr[0]}
+	current := head
+	for i := 1; i < len(arr); i++ {
+		current.Next = &ListNode{Val: arr[i]}
+		current = current.Next
+	}
+	return head
+}
+
+func linkedListToArr(head *ListNode) []int {
+	arr := make([]int, 0)
+	current := head
+
+	for current != nil {
+		arr = append(arr, current.Val)
+		current = current.Next
+	}
+	return arr
+}
+
+// & Working solution
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+
+	current := dummy
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+
 	}
 
+	if l1 != nil {
+		current.Next = l1
+	} else {
+		current.Next = l2
+	}
+
+	return dummy.Next
 }
