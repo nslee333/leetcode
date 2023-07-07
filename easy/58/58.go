02bf519f1acd60b75047904f33534eccf220764f
Nathan <nslee333@gmail.com>
2023-07-07 01:42:29 -0700

    correct second implementation

--- easy/58/58.go
+++ easy/58/58.go
@@ -11,5 +11,21 @@ func lengthOfLastWord(s string) int {
 }
 
 func lengthOfLastWord2(s string) int {
-	return 0
+	str := strings.Split(s, "")
+
+	lastWord := make([]string, 0)
+
+	for i := len(str) - 1; i >= 0; i-- {
+
+		if str[i] == ` ` && len(lastWord) < 1 {
+			continue
+
+		} else if str[i] == ` ` && len(lastWord) >= 1 {
+			break
+
+		} else if str[i] != ` ` {
+			lastWord = append(lastWord, str[i])
+		}
+	}
+	return len(lastWord)
 }
