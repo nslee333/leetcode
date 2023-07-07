cb1b281b21ad57256f1c076cdccd0a83c979da55
Nathan <nslee333@gmail.com>
2023-07-07 07:11:48 -0700

    formatting for readability

--- algorithms/selection_sort/selection_sort.py
+++ algorithms/selection_sort/selection_sort.py
@@ -1,6 +1,7 @@
 def findSmallest(arr):
     smallest = arr[0]
     smallest_index = 0
+    
     for i in range(1, len(arr)):
         
         if arr[i] < smallest:
@@ -11,6 +12,7 @@ def findSmallest(arr):
     
 def selectionSort(arr):
     newArr = []
+    
     for i in range(len(arr)):
         smallest = findSmallest(arr)
         newArr.append(arr.pop(smallest))
