43f148f5a05fddf070b29b77fc7e9ceb1afa8f79
Nathan <nslee333@gmail.com>
2023-07-08 16:54:32 -0700

    added test case

--- easy/69/lc_69_test.py
+++ easy/69/lc_69_test.py
@@ -6,7 +6,15 @@ def runTest():
     got = mySqrt(input_x)
     
     if got != output:
-        print(f"error: got {got}, output {output}, input_x {input_x}")
+        print(f"Ex. 1: error: got {got}, output {output}, input_x {input_x}")
+        
+def runTest():
+    input_x = 8
+    output = 2
+    got = mySqrt(input_x)
+    
+    if got != output:
+        print(f"Ex. 2: error: got {got}, output {output}, input_x {input_x}")
         
 if __name__ == "__main__":
     runTest()
