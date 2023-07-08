c641b156ef796ca372e6c2728bea15f84f44d88e
Nathan <nslee333@gmail.com>
2023-07-08 16:54:27 -0700

    working solution

--- easy/69/lc_69.py
+++ easy/69/lc_69.py
@@ -1,5 +1,16 @@
-
+import math
 
 def mySqrt(x: int) -> int:
-    return x
+    left = 0
+    right = x
+    
+    while left <= right:
+        mid = left + (right - left) // 2
         
+        if mid * mid == x:
+            return mid
+        elif mid * mid > x:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return right
