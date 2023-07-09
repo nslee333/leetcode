807a272e09da386ad89638f52bd4249f8b464751
Nathan <nslee333@gmail.com>
2023-07-08 18:51:49 -0700

    progress save

--- easy/70/lc_70.py
+++ easy/70/lc_70.py
@@ -1,12 +1,13 @@
-
-#
+# 
+# 
 # You are climbing a staircase. It takes n steps to reach the top.
 # 
 # each time you can either climb 1 or 2 steps, in how many distinct ways can you climb to the top?
 # 
-
+# & This problem can be broken down into a fibonacci sequence
 
 
 def climbStairs(n: int) -> int:
-    return n
+    return 0
     
+
