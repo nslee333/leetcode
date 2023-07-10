3737917b71071a6e18999d22605a42ded129a053
Nathan <nslee333@gmail.com>
2023-07-10 16:06:31 -0700

    fixed test

--- easy/83/lc_83_test.py
+++ easy/83/lc_83_test.py
@@ -1,16 +1,17 @@
 from lc_83 import deleteDuplicates
+from lc_83 import ListNode
 
 def runTest():
-    input = [1,1,2]
-    expected = [1,2]
+    input = ListNode(val = 1, next = ListNode(val = 1, next = ListNode(val = 2, next = None)))
+    expected = ListNode(val = 1, next = ListNode(val = 2, next = None))
     outputReturned = deleteDuplicates(input)
     
     if outputReturned != expected:
         print(f"Ex. 1: error: outputReturned {outputReturned}, expected {expected}, input {input}")
         
 def runTest2():
-    input = [1,1,2,3,3]
-    expected = [1,2,3]
+    input =  ListNode(val = 1, next = ListNode(val = 1, next = ListNode(val = 2, next = ListNode(val = 3, next = ListNode(val = 3, next = None)))))
+    expected = ListNode(val = 1, next = ListNode(val = 2, next = ListNode(val = 3, next = None)))
     outputReturned = deleteDuplicates(input)
     
     if outputReturned != expected:
