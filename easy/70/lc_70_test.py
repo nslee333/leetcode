0b100984b8a7bc3614dd6ca721cd58649a5918c9
Nathan <nslee333@gmail.com>
2023-07-10 14:05:54 -0700

    changed test variables for clarity

--- easy/70/lc_70_test.py
+++ easy/70/lc_70_test.py
@@ -2,34 +2,34 @@ from lc_70 import climbStairs
 
 def runTest():
     input = 2
-    output = 2
-    got = climbStairs(input)
-    if got != output:
-        print(f"Ex. 1: error: got {got}, output {output}, input {input}")
+    expected = 2
+    outputReturned = climbStairs(input)
+    if outputReturned != expected:
+        print(f"Ex. 1: error: outputReturned {outputReturned}, expected {expected}, input {input}")
         
 def runTest2():
     input = 3
-    output = 3
-    got = climbStairs(input)
+    expected = 3
+    outputReturned = climbStairs(input)
     
-    if got != output:
-        print(f"Ex. 2: error: got {got}, output {output}, input {input}")
+    if outputReturned != expected:
+        print(f"Ex. 2: error: outputReturned {outputReturned}, expected {expected}, input {input}")
         
 def runTest3():
     input = 8
-    output = 21
-    got = climbStairs(input)
+    expected = 21
+    outputReturned = climbStairs(input)
     
-    if got != output:
-        print(f"Ex. 3: error: got {got}, output {output}, input {input}")
+    if outputReturned != expected:
+        print(f"Ex. 3: error: outputReturned {outputReturned}, expected {expected}, input {input}")
         
 def runTest4():
     input = 10
-    output = 55
-    got = climbStairs(input)
+    expected = 55
+    outputReturned = climbStairs(input)
     
-    if got != output:
-        print(f"Ex. 3: error: got {got}, output {output}, input {input}")
+    if outputReturned != expected:
+        print(f"Ex. 3: error: outputReturned {outputReturned}, expected {expected}, input {input}")
         
 if __name__ == "__main__":
     runTest()
