04d32e1efa4ca6e886ef568a6f8877714f2322a0
Nathan <nslee333@gmail.com>
2023-07-10 17:32:33 -0700

    cleaned up test to just run it

--- easy/83/lc_83_test.py
+++ easy/83/lc_83_test.py
@@ -3,37 +3,24 @@ from lc_83 import ListNode
 
 
 
-def printVals(head: ListNode, num: int):
-    for elem in range(num):
-        print(head.val)
-        if head.next == ListNode: head = head.next
-        if head.next == None: break
-
-
-
-
 def runTest():
     input = ListNode(val = 1, next = ListNode(val = 1, next = ListNode(val = 2, next = None)))
     expected = ListNode(val = 1, next = ListNode(val = 2, next = None))
     outputReturned = deleteDuplicates(input)
     
-
-    print("expected")
-    printVals(expected, 2)
-    print("returned")
-    printVals(outputReturned, 3)
-        
+    
+    
 def runTest2():
     input =  ListNode(val = 1, next = ListNode(val = 1, next = ListNode(val = 2, next = ListNode(val = 3, next = ListNode(val = 3, next = None)))))
     expected = ListNode(val = 1, next = ListNode(val = 2, next = ListNode(val = 3, next = None)))
     outputReturned = deleteDuplicates(input)
     
+def runTest3():
+    input =  ListNode(val = 1, next = ListNode(val = 1, next = ListNode(val = 1, next = None)))
+    expected = ListNode(val = 1, next = None)
+    outputReturned = deleteDuplicates(input)
     
-    print("expected")
-    printVals(expected, 3)
-    print("returned")
-    printVals(outputReturned, 3)
-
 if __name__ == "__main__":
     runTest()
     runTest2()
+    runTest3()
