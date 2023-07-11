090ad322404e3a62c18f398126dfcddfd52b7ca0
Nathan <nslee333@gmail.com>
2023-07-10 17:32:48 -0700

    Working sol

--- easy/83/lc_83.py
+++ easy/83/lc_83.py
@@ -1,3 +1,10 @@
+# 
+# Given the head of a sorted linked list
+# Delete all duplicates such that each element
+# appears only once.
+# 
+# Returned the linked list sorted as well.
+# 
 
 
 class ListNode:
@@ -6,6 +13,12 @@ class ListNode:
         self.next = next
 
 
-
-def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
-    return ListNode
+def deleteDuplicates(head: ListNode) -> ListNode:
+    current = head
+                
+    while current is not None and current.next is not None:
+        if current.val == current.next.val:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head    
