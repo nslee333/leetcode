43da0cac742aa8379efa82276068cd24d64b6f27
Nathan <nslee333@gmail.com>
2023-07-10 17:18:29 -0700

    don't take this test code seriously :)

--- easy/83/lc_83_test.py
+++ easy/83/lc_83_test.py
@@ -1,21 +1,38 @@
 from lc_83 import deleteDuplicates
 from lc_83 import ListNode
 
+
+
+def printVals(head: ListNode, num: int):
+    for elem in range(num):
+        print(head.val)
+        if head.next == ListNode: head = head.next
+        if head.next == None: break
+
+
+
+
 def runTest():
     input = ListNode(val = 1, next = ListNode(val = 1, next = ListNode(val = 2, next = None)))
     expected = ListNode(val = 1, next = ListNode(val = 2, next = None))
     outputReturned = deleteDuplicates(input)
     
-    if outputReturned != expected:
-        print(f"Ex. 1: error: outputReturned {outputReturned}, expected {expected}, input {input}")
+
+    print("expected")
+    printVals(expected, 2)
+    print("returned")
+    printVals(outputReturned, 3)
         
 def runTest2():
     input =  ListNode(val = 1, next = ListNode(val = 1, next = ListNode(val = 2, next = ListNode(val = 3, next = ListNode(val = 3, next = None)))))
     expected = ListNode(val = 1, next = ListNode(val = 2, next = ListNode(val = 3, next = None)))
     outputReturned = deleteDuplicates(input)
     
-    if outputReturned != expected:
-        print(f"Ex. 2: error: outputReturned {outputReturned}, expected {expected}, input {input}")
+    
+    print("expected")
+    printVals(expected, 3)
+    print("returned")
+    printVals(outputReturned, 3)
 
 if __name__ == "__main__":
     runTest()
