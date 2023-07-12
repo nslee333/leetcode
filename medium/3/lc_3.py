f3c852f022ab630127381d6196441de81bdf3cf1
Nathan <nslee333@gmail.com>
2023-07-11 18:24:33 -0700

    working sol

--- medium/3/lc_3.py
+++ medium/3/lc_3.py
@@ -5,4 +5,19 @@ Given a string s, find the length of the longest substring without repeating cha
 """
 
 def lengthOfLongestSubstring(s: str) -> int:
-    return 0
+    n = len(s)
+    start = 0
+    end = 0
+    maxLen = 0
+    seen = set()
+
+    while end < n:
+        if s[end] not in seen:
+            seen.add(s[end])
+            end += 1
+            maxLen = max(maxLen, end - start)
+        else:
+            seen.remove(s[start])
+            start += 1
+    return maxLen
+    
