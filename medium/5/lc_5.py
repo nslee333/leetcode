013f1f8bbebc951abf5d796c5c344694ed1a2d8d
Nathan <nslee333@gmail.com>
2023-07-12 11:17:05 -0700

    switch to python's unittest

---
+++ medium/5/lc_5.py
@@ -0,0 +1,45 @@
+"""
+Leetcode #5.
+
+given a string s, return the longest palindromic substring in s.
+
+"""
+
+import unittest
+
+
+class Solution(object):
+    
+    def longestPalindrome(self, s):
+        return ""
+    
+    
+    
+
+class Test(unittest.TestCase):
+    def test(self):
+        s = Solution()
+        
+        for i, e in [("string", "string")]:
+            
+            if self.assertEqual(s.longestPalindrome(i), e) == False:
+                print(i,e)
+                
+    def test2(self):
+        s = Solution()
+        
+        for i, e in [("string", "string")]:
+            
+            if self.assertEqual(s.longestPalindrome(i), e) == False:
+                print(i,e)
+
+    def test3(self):
+        s = Solution()
+        
+        for i, e in [("string", "string")]:
+            
+            if self.assertEqual(s.longestPalindrome(i), e) == False:
+                print(i,e)
+    
+if __name__ == '__main__':
+    unittest.main()
