e649f5ae60d1f93e490c64efd3e9b3aabdea0e30
Nathan <nslee333@gmail.com>
2023-07-14 15:41:00 -0700

    working solution

--- easy/lc_383.py
+++ easy/lc_383.py
@@ -14,13 +14,65 @@ Given two strings randsomNote and magazine,
 Each letter can only be used once in randomNote
 
 """
+"""
+Thought Process:
 
+1. We need to check if the letters from magazine are in random note.
+2. each letter in magizine can only be used once.
 
+We need to return true or false if we can contruct the random note from the magazine.
+
+We can't use a list because magazine can have duplicate characters.
+
+We might be able to use a hash table for quick lookup.
+
+pesudocode:
+
+convert magazine into an array.
+
+init a hash table
+
+loop over magazine array
+    we push index i as the key and arr[index] as the value to the hash table
+    ^ I don't think above will necessarily work - 
+
+convert random note into array
+
+loop over the randsomNote array
+    if i 
+    
+My though process got lost as I ran into a road block with not knowing what I can do with hash maps
+"""
 
 
 class Solution:
    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-       return False
+        
+        # This solution has an O(n + m) time complexity -> where n is the length of the magazine and and m is the length of the 
+        # note
+        
+        mag = list(magazine)
+        note = list(ransomNote)
+        
+        if len(note) > len(mag):
+            return False
+        
+        hash = {}
+                
+        for letter in mag:
+            if letter not in hash:
+                hash[letter] = 1
+            else:
+                hash[letter] += 1
+    
+        for letter in note:
+            if letter not in hash:
+                return False
+            elif hash.get(letter) >= 1:
+                hash[letter] -= 1
+            else:
+                return False
+        return True            
         
 
 
