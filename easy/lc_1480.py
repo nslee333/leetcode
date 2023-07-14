d99c7d560013b2fa0bb93089bbb0e03e16b1b104
Nathan <nslee333@gmail.com>
2023-07-13 19:30:30 -0700

    Sol

---
+++ easy/lc_1480.py
@@ -0,0 +1,54 @@
+import unittest
+
+"""
+Leetcode 1480
+
+Given an array nums. We define a running sum of an array as runnungSum[i] = sum(nums[0]...nums[i])
+
+return the running sum of nums
+
+"""
+
+class Solution:
+    def runningSum(self, nums):
+        count = []
+        count.append(nums[0])
+
+        for i in range(len(nums)):
+            if i < len(nums)-1:
+                count.append(count[i] + nums[i+1])
+
+        return count
+
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        s = Solution()
+        
+        input = [1,2,3,4]
+        expected = [1,3,6,10]
+        output = s.runningSum(input)
+        
+        self.assertEqual(output, expected)
+                
+    def test2(self):
+        s = Solution()
+        
+        input = [1,1,1,1,1]
+        expected = [1,2,3,4,5]
+        output = s.runningSum(input)
+        
+        self.assertEqual(output, expected)
+        
+    def test3(self):
+        s = Solution()
+        
+        input = [3,1,2,10,1]
+        expected = [3,4,6,16,17]
+        output = s.runningSum(input)
+        
+        self.assertEqual(output, expected)
+
+if __name__ == '__main__':
+    unittest.main()
