4ca51416a5e1e9b4d75139e51763858ef5ce075c
Nathan <nslee333@gmail.com>
2023-07-18 16:10:05 -0700

    setup for 136

---
+++ easy/lc_136.py
@@ -0,0 +1,61 @@
+import unittest
+
+
+"""
+# 136. Single Number
+
+Given a non-empty array of integers nums, every element appears twice execpt for one.
+
+Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space
+
+
+"""
+
+
+
+
+
+class Solution:
+    def single_number(self, nums):
+        return 0
+
+
+
+
+
+
+
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        s = Solution()
+        
+        input = [2,2,1]
+        expected = 1
+        output = s.single_number(input)
+        
+        self.assertEqual(output, expected)
+                
+    def test2(self):
+        s = Solution()
+        
+        input = [4,1,2,1,2]
+        expected = 4
+        output = s.single_number(input)
+        
+        self.assertEqual(output, expected)
+
+    def test3(self):
+        s = Solution()
+        
+        input = [1]
+        expected = 1
+        output = s.single_number(input)
+        
+        self.assertEqual(output, expected)
+
+if __name__ == '__main__':
+    unittest.main()
