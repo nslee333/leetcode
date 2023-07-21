b654ff06ced5d57e0c1ed6af48058828a0574abb
Nathan <nslee333@gmail.com>
2023-07-21 14:32:51 -0700

    hashset solution

--- easy/lc_217.py
+++ easy/lc_217.py
@@ -15,13 +15,9 @@ and return false if every element is distinct.
 class Solution:
     def contains_duplicate(self, nums):
         result = bool
-        
-        # [1,2,3,4,5]
-        
+                
         nums.sort()
-        
-        print(nums)
-        
+            
         for i in range(len(nums)):
             if i < len(nums)-1:
                 if nums[i] == nums[i+1]:
@@ -31,11 +27,17 @@ class Solution:
             result = False
                     
         return result
+    
+    
+    def contains_duplicate_hashset(self, nums):
+        hashset = set()
 
-
-
-
-
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+            
 
 
 
@@ -64,6 +66,33 @@ class Test(unittest.TestCase):
         input = [1,1,1,3,3,4,3,2,4,2]
         expected = True
         output = s.contains_duplicate(input)
+        
+        self.assertEqual(output, expected)
+        
+    def test4(self):
+        s = Solution()
+        
+        input = [1,2,3,1]
+        expected = True
+        output = s.contains_duplicate_hashset(input)
+        
+        self.assertEqual(output, expected)
+                
+    def test5(self):
+        s = Solution()
+        
+        input = [1,2,3,4]
+        expected = False
+        output = s.contains_duplicate_hashset(input)
+        
+        self.assertEqual(output, expected)
+        
+    def test6(self):
+        s = Solution()
+        
+        input = [1,1,1,3,3,4,3,2,4,2]
+        expected = True
+        output = s.contains_duplicate_hashset(input)
         
         self.assertEqual(output, expected)
 
