1ac0fe0fe80f72f19af43ecabc140735958cc4a5
Nathan <nslee333@gmail.com>
2023-07-21 14:03:06 -0700

    working sol

---
+++ easy/lc_217.py
@@ -0,0 +1,77 @@
+import unittest
+
+
+"""
+Leetcode 217
+
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+
+"""
+
+
+
+class Solution:
+    def contains_duplicate(self, nums):
+        result = bool
+        
+        # [1,2,3,4,5]
+        
+        nums.sort()
+        
+        print(nums)
+        
+        for i in range(len(nums)):
+            if i < len(nums)-1:
+                if nums[i] == nums[i+1]:
+                    result = True
+            
+        if result == bool:
+            result = False
+            
+
+        
+        
+        return result
+
+
+
+
+
+
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        s = Solution()
+        
+        input = [1,2,3,1]
+        expected = True
+        output = s.contains_duplicate(input)
+        
+        self.assertEqual(output, expected)
+                
+    def test2(self):
+        s = Solution()
+        
+        input = [1,2,3,4]
+        expected = False
+        output = s.contains_duplicate(input)
+        
+        self.assertEqual(output, expected)
+        
+    def test3(self):
+        s = Solution()
+        
+        input = [1,1,1,3,3,4,3,2,4,2]
+        expected = True
+        output = s.contains_duplicate(input)
+        
+        self.assertEqual(output, expected)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
