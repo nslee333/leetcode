e43e5a8932cab6ae4915d0b20f40eb4491848bb6
Nathan <nslee333@gmail.com>
2023-07-20 19:18:35 -0700

    WIP

--- easy/lc_136.py
+++ easy/lc_136.py
@@ -18,33 +18,27 @@ You must implement a solution with a linear runtime complexity and use only cons
 
 class Solution:
     def single_number(self, nums):
-        # print(nums)
         result = 0
         hash = {}
         
-        # print(len(nums))
+        # [3, 2]
+        # Counting instances of numbers in nums
         for i in range(len(nums)):
-            # print(i)
-            # print(nums[i], "HIH")
-            if hash.get(i) == None:
-                hash[i] = 1 # key index to value int
-            if hash.get(i) == nums[i]:
-                hash[i] += 1
-                print(i, "i")
-                print(hash[i], "hash[i]")
-            
-        for j in range(len(nums)):
-            # print(j)
-            if hash.get(j) == nums[j]:
-                hash[j] -= 1
-                print(hash[j], "hash[j]")
-                print(j, "j")
-            
-        for h in hash:
-            if h == 1:
-                # print(nums[h], "result")
-                # print(h, "h")
-                result = nums[h]
+            if hash.get(nums[i]) == None:
+                hash[nums[i]] = 1
+                
+            if hash.get(nums[i]) != None:
+                hash[nums[i]] += 1
+        
+        for i in range(len(hash)):
+            print(hash.get(nums[i]), "numsi")
+            print(hash.get(i), "i")
+            if hash.get(nums[i]) == 1:
+                result = i
+                  
+        
+
+
         return result
 
 
