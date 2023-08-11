aed32f24ec347989de8a711f2dc5ec5ef64d2664
Nathan <nslee333@gmail.com>
2023-08-11 10:56:16 -0700

    added another solution

--- medium/lc_739.py
+++ medium/lc_739.py
@@ -10,3 +10,16 @@ class Solution:
             stack.append(i)
         return result
     
+# same concept but using enumerate, above is simpler.
+
+# class Solution:
+# def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+#     stack = []
+#     result = [0] * len(temperatures)
+
+#     for i, t in enumerate(temperatures):
+#         while stack and t > stack[-1][0]:
+#             stack_t, stack_i = stack.pop()
+#             result[stack_i] = (i - stack_i)
+#         stack.append([t, i])
+#     return result
