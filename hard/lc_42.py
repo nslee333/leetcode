4b5eb2b6222ca2e1e6eb41a8ff4ded2cc1affbfc
Nathan <nslee333@gmail.com>
2023-08-26 16:25:15 -0700

    Added neetcode solution

--- hard/lc_42.py
+++ hard/lc_42.py
@@ -26,3 +26,22 @@ class Solution:
         return res
     
     
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         if not height: return 0
+        
+        
+#         l, r = 0, len(height) - 1
+#         max_left, max_right = height[l], height[r]
+        
+#         while l < r:
+#             if max_left < max_right:
+#                 l += 1
+#                 max_left = max(max_left, height[l])
+#                 res += max_left - height[l]
+                
+#             else:
+#                 r -= 1
+#                 max_right = max(max_right, height[r])
+#                 res += max_right - height[r]
+#         return res
