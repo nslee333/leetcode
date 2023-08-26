1c26a62110ff2090a75e0f702275daef5044ae10
Nathan <nslee333@gmail.com>
2023-08-26 10:48:14 -0700

    added neetcode solution

--- medium/lc_11.py
+++ medium/lc_11.py
@@ -36,6 +36,20 @@ class Solution:
         return largest_area
 
 
+# neetcode solution
+# class Solution:
+#     def maxArea(self, height):
+#         l, r = 0, len(height) - 1
+#         res = 0
+        
+#         while l < r:
+#             res = max(res, min(height[l], height[r]) * (r - l))
+#             if height[l] < height[r]:
+#                 l += 1
+#             elif height[r] <= height[l]:
+#                 r -= 1
+#         return res
+
 class Test(unittest.TestCase):
     def test(self):
         s = Solution()
