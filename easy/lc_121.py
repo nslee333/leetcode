3895329ce3b5cb7edb02c89d8aacf9a4ebb1374a
Nathan <nslee333@gmail.com>
2023-09-04 13:42:01 -0700

    added Neetcode solutions

--- easy/lc_121.py
+++ easy/lc_121.py
@@ -1,16 +1,34 @@
+# ^ Neetcode #1
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        
-        
-        
-        
-        
-        # Brute force algorithm, o(N^2) time complexity
-        # ! this solution is too slow.
-        # max_profit = 0
-        # for i in range(len(prices)):
-        #     for j in range(i + 1, len(prices)):
-        #         current = prices[j] - prices[i]
-        #         max_profit = max(max_profit, current)
-        # return max_profit
+        l, r = 0, 1
+        max_profit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                current = prices[r] - prices[l]
+                max_profit = max(max_profit, current)
+            else:
+                l = r
+            r += 1
+        return max_profit
+
+# ^ Neetcode #2 - 100ms faster than 
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+    # res = 0
+    #     lowest = prices[0]
+    #     for price in prices:
+    #         if price < lowest:
+    #             lowest = price
+    #         res = max(res, price - lowest)
+    #     return res
+    
+#  ^ My brute force idea, o(N^2) time complexity
+# ! this idea is too slow, not a solution
+# max_profit = 0
+# for i in range(len(prices)):
+#     for j in range(i + 1, len(prices)):
+#         current = prices[j] - prices[i]
+#         max_profit = max(max_profit, current)
+# return max_profit
