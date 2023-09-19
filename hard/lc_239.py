74e9ae4c191b807ff73235c0aaf0ee2528e0012f
Nathan <nslee333@gmail.com>
2023-09-18 23:28:44 -0700

    commit

--- hard/lc_239.py
+++ hard/lc_239.py
@@ -1,28 +1,21 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-
-        # 1. Find the largest value in the window.
-
-        # 2. Build and return an array of largest window values
-
-        # intuition:
-        # Looking in the window, we loop through the window, and we push the largest number
-
-        # l = 0
-        # r = l
         
         res = []
+        dq = collections.deque()
 
-        for l in range(len(nums)):
-            temp_max = nums[l] 
-            r = l
-            while (r - l) < k and r < len(nums):
-                temp_max = max(nums[r], temp_max)
-                r += 1
-            if (r - l) == k:
-                res.append(temp_max)
-        return res
+        l = r = 0
+
+        while r < len(nums):
+            while dq and nums[dq[-1]] < nums[r]:
+                dq.pop()
+            dq.append(r)
+
+            if l > dq[0]:
+                dq.popleft()
             
-        
-
-        
+            if (r + 1) >= k:
+                res.append(nums[dq[0]])
+                l += 1
+            r += 1
+        return res
