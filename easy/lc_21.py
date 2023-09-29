6f94cffd48e34538e1525dc8017494ace3ee1942
Nathan <nslee333@gmail.com>
2023-09-28 18:51:04 -0700

    added improved solution

--- easy/lc_21.py
+++ easy/lc_21.py
@@ -45,47 +45,90 @@ def convert(ll):
 
 # & Faster Python Solution
 
-# # Definition for singly-linked list.
-# # class ListNode:
-# #     def __init__(self, val=0, next=None):
-# #         self.val = val
-# #         self.next = next
-# class Solution:
-#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-#         l_1, l_2 = list1, list2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l_1, l_2 = list1, list2
 
-#         if not l_1 and not l_2:
-#             return 
+        if not l_1 and not l_2:
+            return 
         
-#         res = ListNode()
-#         t_res = res
+        res = ListNode()
+        t_res = res
 
-#         while True:
-#             if not l_1 and not l_2:
-#                 break
+        while True:
+            if not l_1 and not l_2:
+                break
             
-#             if not l_1:
-#                 t_res.val = l_2.val
-#                 l_2 = l_2.next
+            if not l_1:
+                t_res.val = l_2.val
+                l_2 = l_2.next
 
-#             elif not l_2:
-#                 t_res.val = l_1.val
-#                 l_1 = l_1.next
+            elif not l_2:
+                t_res.val = l_1.val
+                l_1 = l_1.next
 
-#             if l_1 and l_2:
-#                 if l_1.val <= l_2.val:
-#                     t_res.val = l_1.val
-#                     l_1 = l_1.next
+            if l_1 and l_2:
+                if l_1.val <= l_2.val:
+                    t_res.val = l_1.val
+                    l_1 = l_1.next
 
 
-#                 elif l_1.val > l_2.val:
-#                     t_res.val = l_2.val
-#                     l_2 = l_2.next
+                elif l_1.val > l_2.val:
+                    t_res.val = l_2.val
+                    l_2 = l_2.next
 
-#             if l_1 or l_2:
-#                 t_res.next = ListNode()
-#                 t_res = t_res.next
-#             else:
-#                 t_res.next = None
+            if l_1 or l_2:
+                t_res.next = ListNode()
+                t_res = t_res.next
+            else:
+                t_res.next = None
 
-#         return res
+        return res
+
+
+# & Improved solution for above.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l_1, l_2 = list1, list2
+
+        if not l_1 and not l_2:
+            return 
+        
+        res = ListNode()
+        t_res = res
+
+        while l_1 or l_2:
+            
+            if not l_1:
+                t_res.val = l_2.val
+                l_2 = l_2.next
+
+            elif not l_2:
+                t_res.val = l_1.val
+                l_1 = l_1.next
+
+            elif l_1 and l_2:
+                if l_1.val <= l_2.val:
+                    t_res.val = l_1.val
+                    l_1 = l_1.next
+
+                elif l_1.val > l_2.val:
+                    t_res.val = l_2.val
+                    l_2 = l_2.next
+
+            if l_1 or l_2:
+                t_res.next = ListNode()
+                t_res = t_res.next
+
+        return res
