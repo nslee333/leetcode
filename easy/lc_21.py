84aa4c51c4d664996bf3b08dd28c6c3f4567c980
Nathan <nslee333@gmail.com>
2023-09-28 18:31:45 -0700

    Added faster solution

--- easy/lc_21.py
+++ easy/lc_21.py
@@ -43,8 +43,49 @@ def convert(ll):
     return res
 
 
+# & Faster Python Solution
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         l_1, l_2 = list1, list2
+
+#         if not l_1 and not l_2:
+#             return 
+        
+#         res = ListNode()
+#         t_res = res
+
+#         while True:
+#             if not l_1 and not l_2:
+#                 break
+            
+#             if not l_1:
+#                 t_res.val = l_2.val
+#                 l_2 = l_2.next
+
+#             elif not l_2:
+#                 t_res.val = l_1.val
+#                 l_1 = l_1.next
+
+#             if l_1 and l_2:
+#                 if l_1.val <= l_2.val:
+#                     t_res.val = l_1.val
+#                     l_1 = l_1.next
 
 
+#                 elif l_1.val > l_2.val:
+#                     t_res.val = l_2.val
+#                     l_2 = l_2.next
 
+#             if l_1 or l_2:
+#                 t_res.next = ListNode()
+#                 t_res = t_res.next
+#             else:
+#                 t_res.next = None
 
-
+#         return res
