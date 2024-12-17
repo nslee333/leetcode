452c29db018093462523f562082e29fbd11f472f
Nathan <nslee333@gmail.com>
2024-12-17 08:50:20 -0800

    second problem

---
+++ arrays_and_hashing/2_242.py
@@ -0,0 +1,11 @@
+
+"""
+    Given two strings s and t, return true if t is an anagram
+    of s, and false otherwise.
+"""
+
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
