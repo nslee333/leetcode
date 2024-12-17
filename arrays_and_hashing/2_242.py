2f989fafc3c362921ec3a0fae0375c0a3c0647ed
Nathan <nslee333@gmail.com>
2024-12-17 08:56:45 -0800

    another one

--- arrays_and_hashing/2_242.py
+++ arrays_and_hashing/2_242.py
@@ -9,3 +9,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
