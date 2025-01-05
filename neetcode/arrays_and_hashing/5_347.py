f3064c24c75c55211f8e7a50bb51b0da1c01cfb9
Nathan <nslee333@gmail.com>
2025-01-05 10:14:20 -0800

    5

--- neetcode/arrays_and_hashing/5_347.py
+++ neetcode/arrays_and_hashing/5_347.py
@@ -24,3 +24,50 @@ class Solution:
                 return result2
             result2.append(key)
 # this isn't solved yet
+
+
+# neetcode
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # loop through array
+
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
+# my modified solution
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # loop through array
+
+        # we need to 
+        count = {}
+
+        freq = [[] for i in range(len(nums) + 1)]
+        
+        res = []
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        print(freq)
+
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+            
+
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
