724c4e1a3bb7104a22feefcb9dc7907b23001fb2
Nathan <nslee333@gmail.com>
2025-02-01 16:44:04 -0800

    mo better 167

--- neetcode/two_pointer/11_167.py
+++ neetcode/two_pointer/11_167.py
@@ -1,5 +1,62 @@
 
 
+# 5 MS
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        result = []
+
+
+        i, j = 0, len(numbers) - 1
+
+        # changing != to < 
+        while i < j:
+            guess = numbers[i] + numbers[j]
+
+            if guess == target:
+                result = [i+1, j+1]
+                return result
+            elif guess > target:
+                j -= 1
+            elif guess < target:
+                i += 1
+
+        return result
+        
+
+
+
+
+
+
+
+
+
+# acceptable solution, 55ms, o n time, 0 1 space
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        result = []
+
+
+        i, j = 0, len(numbers) - 1=
+
+        while i != j:
+            print(i, j)
+            guess = numbers[i] + numbers[j]
+            print(guess)
+
+            if guess > target:
+                j -= 1
+            elif guess < target:
+                i += 1
+            elif guess == target:
+                result = [i+1, j+1]
+                return result
+        return result
+
+
+
+
+
 
 # unefficient solution, O N^2
 class Solution:
