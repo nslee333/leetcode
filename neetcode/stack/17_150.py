b49c2d5185fc4f6f325287e11a38b328aebb3cb0
Nathan <nslee333@gmail.com>
2025-02-03 13:16:50 -0800

    lc 150

---
+++ neetcode/stack/17_150.py
@@ -0,0 +1,26 @@
+
+# pretty good
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        operands = {'+', '-', '/', '*'}
+
+        for i in range(len(tokens)):
+            if tokens[i] in operands:
+                y = int(stack.pop())
+                x = int(stack.pop())
+
+                if tokens[i] == '+':
+                    temp = x + y
+                elif tokens[i] == '-':
+                    temp = x - y 
+                elif tokens[i] == '/':
+                    temp = x / y
+                elif tokens[i] == '*':
+                    temp = x * y
+                stack.append(temp)
+
+                
+            else:
+                stack.append(tokens[i])
+        return int(stack[-1])
