163acc9872711c6fd26e1c9abbe50f064c097003
Nathan <nslee333@gmail.com>
2025-02-03 14:33:49 -0800

    another one

--- neetcode/3_sliding_window/15_121.py
+++ neetcode/3_sliding_window/15_121.py
@@ -1,11 +1,20 @@
 
+# sliding window solution
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        l = 0
+        r = l + 1
+        max_value = 0
 
-
-
-
-
-
-
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_value = max(max_value, profit)
+            else:
+                l = r
+            r += 1
+        return max_value
 
 
 # neetcode dynamic programming solution
