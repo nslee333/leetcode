6bebefaf7c929d73ff5799972bde4c05087b0723
Nathan <nslee333@gmail.com>
2025-02-03 11:37:23 -0800

    added 155

---
+++ neetcode/stack/16_155.py
@@ -0,0 +1,64 @@
+
+# neetcode solution
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(val)
+        
+
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+
+
+
+# my solution, slow
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop(-1)
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+        
+
+    def getMin(self) -> int:
+        return min(self.stack) if len(self.stack) > 0 else 0
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
