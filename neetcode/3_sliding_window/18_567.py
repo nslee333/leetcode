783b3a84cbe3473f88ce2a149b52b5a4a5bc2bd9
Nathan <nslee333@gmail.com>
2025-02-08 09:02:37 -0800

    another solution for 567

--- neetcode/3_sliding_window/18_567.py
+++ neetcode/3_sliding_window/18_567.py
@@ -1,3 +1,39 @@
+
+# this one is much better, 19ms runtime, the sorting in the old one was horrid
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        s1_count = {}
+        s2_count = {}
+
+
+        for j in range(len(s1)):
+            s1_count[s1[j]] = s1_count.get(s1[j], 0) + 1
+
+
+        l = 0
+        
+
+        for r in range(len(s2)):
+            while (r - l + 1) > len(s1):
+                if s2_count.get(s2[l]) > 1:
+                    s2_count[s2[l]] -= 1
+                else:
+                    s2_count.pop(s2[l])
+                l += 1
+            s2_count[s2[r]] = s2_count.get(s2[r], 0) + 1
+
+            if s1_count == s2_count: return True
+
+        return s1_count == s2_count
+
+
+
+
+
+
+
+
 # dog slow LOL, like 3045MS 😅 can't believe it passed LMAO
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
