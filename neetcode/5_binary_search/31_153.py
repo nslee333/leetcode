60332dc9346ce0aa090160d1273b44d963e42ad9
Nathan <nslee333@gmail.com>
2025-02-19 14:34:55 -0800

    find min in rotated array

--- neetcode/5_binary_search/31_153.py
+++ neetcode/5_binary_search/31_153.py
@@ -1,4 +1,78 @@
 
+# neetcode solution, O(log n) time, O(1) space
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+
+# more elegant kinda
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        l, r = 0, len(nums) - 1
+        res = float('inf')
+
+        while l <= r:
+            mid = (l + r) // 2
+            
+            mid_guess = nums[mid]
+            left_guess = nums[l]
+            right_guess = nums[r]
+
+            if left_guess < right_guess:
+                res = min(res, left_guess)
+                break
+            
+            res = min(res,mid_guess)
+            
+            if mid_guess >= left_guess:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return res
+
+# my working solution, with neetcode pivot idea, not super elegant
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        l, r = 0, len(nums) - 1
+        res = float('inf')
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            
+            mid_guess = nums[mid]
+            left_guess = nums[l]
+            right_guess = nums[r]
+            
+            print(left_guess, mid_guess)
+            
+            if mid_guess >= left_guess:
+                res = min(res, left_guess)
+                l = mid + 1
+                
+            else:
+                res = min(res, right_guess)
+                r = mid - 1
+            res = min(res,mid_guess)
+                
+        
+        return res
+
 # this passes Leetcode, but it's supposed to be O(log n) runtime, this is O(n log n) runtime
 class Solution:
     def findMin(self, nums: List[int]) -> int:
