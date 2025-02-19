d43ef5cf98fb90473ed9179461cdaf3cffe2424e
Nathan <nslee333@gmail.com>
2025-02-19 09:30:18 -0800

    another solution to 2d matrix problem

--- neetcode/5_binary_search/29_74.py
+++ neetcode/5_binary_search/29_74.py
@@ -1,5 +1,51 @@
 
-# so technically this passes the test cases for leetcode with a O(n) time complexity
+# my solution with chatgpt helping me in finding bugs in it
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        def search(self, array: List[int]) -> (bool, str):
+            left, right = 0, len(array) - 1
+
+            split = (len(array) - 1) // 2
+         
+            while left <= right:
+                split = left + (right - left) // 2
+                
+                if array[split] < target:
+                    left = split + 1
+                elif array[split] > target:
+                    right = split - 1
+                else:
+                    return True, 'none'
+
+            if left > len(array) // 2:
+                return False, 'higher'
+            else:
+                return False, 'lower'
+            
+        if not matrix or not matrix[0]:
+            return False
+
+        l, r = 0, len(matrix) - 1
+        
+        while l <= r:
+            
+            split_m = l + (r - l) // 2
+            result = search(self, matrix[split_m])
+
+            if result[0] == True:
+                return True
+            elif result[0] == False and result[1] == 'higher':
+                
+                l = split_m + 1
+            elif result[0] == False and result[1] == 'lower':
+                r = split_m - 1
+
+            
+        return False
+
+
+# so technically this flattening approach passes the test cases for leetcode with a O(n) time complexity
 
 # but it demands a log(n * m) time complexity solution, so technially this doesn't count
 class Solution:
@@ -25,4 +71,3 @@ class Solution:
             else:
                 return True
         return False
-        
