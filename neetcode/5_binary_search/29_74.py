4ed22720f4df7a112867caf8fc403d1464f5feaa
Nathan <nslee333@gmail.com>
2025-02-19 09:55:00 -0800

    neetcode solution

--- neetcode/5_binary_search/29_74.py
+++ neetcode/5_binary_search/29_74.py
@@ -1,4 +1,35 @@
 
+
+# neetcode solution, O(log n + log m) space O(1)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
+
 # my solution with chatgpt helping me in finding bugs in it
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
