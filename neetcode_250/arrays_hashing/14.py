1f10f455f34008082943bb8b2a32feb59fa5e8c5
Nathan <nslee333@gmail.com>
2025-03-06 15:53:18 -0800

    longest common prefix

---
+++ neetcode_250/arrays_hashing/14.py
@@ -0,0 +1,36 @@
+# longest common prefix
+
+# my solution
+# linear time O(n * m) and linear space O(n)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+        
+        
+        # zip takes all chars at i and puts them into a tuple
+        res = ""
+        for chars in zip(*strs):
+            # set is used for uniqueness, if more than one, then the prefix is broken
+            if len(set(chars)) != 1:
+                return res
+            res += chars[0]
+        return res
+    
+    
+# neetcode vertical scanning linear time & space
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+
+        # scanning through first string
+        for i in range(len(strs[0])):
+            # for each string
+            for s in strs:
+                # if i is out of bounds or current string does not equal current prefix
+                # return res 
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        
+        return res
