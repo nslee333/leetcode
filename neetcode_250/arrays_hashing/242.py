271c2bc99b4d185fbd170687ee9d2cf80689d89a
Nathan <nslee333@gmail.com>
2025-03-06 14:50:30 -0800

    optimal solution for valid anagrams

--- neetcode_250/arrays_hashing/242.py
+++ neetcode_250/arrays_hashing/242.py
@@ -1,5 +1,34 @@
 # valid anagram
 
+# optimal hash table solution
+
+
+# linear time O(n + m) & constant space (since 26 letters in alphabet)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:     
+        
+        if len(s) != len(t):
+            return False
+
+        # init an  array of 26 characters (english alphabet)
+        count = [0] * 26
+        print(count)
+        
+        
+        # as you iterate, add 1 for each character for s
+        # subtract 1 for each char in t
+        
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        # if anagrams, it'll even out to zero
+        for item in count:
+            if item > 0:
+                return False
+
+        return True
+
 
 # simplist solution, log linear time O(n log n + m log m), linear space O(n + m)
 # for sorted() (timsort)
