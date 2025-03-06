a074c20d1e17403d11e4849458ff66b6f9f3f28d
Nathan <nslee333@gmail.com>
2025-03-06 11:48:50 -0800

    comments on solution

--- neetcode_250/arrays_hashing/217.py
+++ neetcode_250/arrays_hashing/217.py
@@ -1,3 +1,6 @@
+
+# linear time O(N), because we loop through each element
+# space is linear as well, since the worst case is that we have to store N elements
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         items = set()
