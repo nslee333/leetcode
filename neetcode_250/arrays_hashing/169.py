c09d3ae302d4af9b183cb6580d845d8b8f9cb3c4
Nathan <nslee333@gmail.com>
2025-03-07 14:31:45 -0800

    find majority element

--- neetcode_250/arrays_hashing/169.py
+++ neetcode_250/arrays_hashing/169.py
@@ -1,6 +1,39 @@
 
 
 
+# neetcode boyer moore voting algorithm, linear time and constant space
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res, count = float('-inf'), 0
+
+        for n in nums:
+            if n == res:
+                count += 1
+            elif n != res and count == 0:
+                res = n
+                count = 1
+            else: 
+                count -= 1
+        
+        return res
+
+# neetcode hash table soltution
+
+# keep a steady count of element and count as you go, then return res
+
+# faste
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        table = {}
+
+        res, max_count = 0, 0
+    
+        for n in nums:
+            table[n] = 1 + table.get(n, 0)
+            res = n if table[n] > max_count else res
+            max_count = max(table[n], max_count)
+        return res
 
 
 # not optimal
@@ -14,6 +47,8 @@ class Solution:
         for item in nums:
             table[item] = 1 + table.get(item, 0)
         return max(table, key=table.get)
+    
+    
     
 # without one-liner
 class Solution:
