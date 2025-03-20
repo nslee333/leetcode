23c7a6ae0aaacc1c56d7e78fc9aeb781a3c984a9
Nathan <nslee333@gmail.com>
2025-03-20 11:58:43 -0700

    encode & decode strings

---
+++ neetcode_250/arrays_hashing/271.py
@@ -0,0 +1,21 @@
+
+
+# I was able to make this work completely on accident
+# linear space & time
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for item in strs:
+            res += (item + "\u000c")
+        print(res, "jgfj")
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = s.split("\u000c")
+        
+        # this was a hacky fix to remove my delimter, and it solved some hard edge cases on accident
+        if res[-1] == '':
+            res = res[:len(res) - 1]
+        
+        return res
