d8bbcdef37d81fcc8b2b2d683e7545f6159755be
Nathan <nslee333@gmail.com>
2025-03-22 12:34:40 -0700

    neetcode sol for find the index of first occurance in a string

--- neetcode_250/arrays_hashing/28.py
+++ neetcode_250/arrays_hashing/28.py
@@ -1,5 +1,20 @@
+# neetcode bruteforce solution, this is linear time O(N * m) and space is constant since we aren't using anything.
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
 
+        n, m = len(haystack), len(needle)
+        for i in range(n - m + 1): # the range is the size of the window 
+            j = 0
+            while j < m: # grow j until it's the length if the needle
+                if haystack[i + j] != needle[j]: # 
+                    break
+                j += 1
+            if j == m:
+                return i
+        return -1
 # my brain is tired, so we're calling it.
+
+# this attempt was using a hashtable, but that's a flawed approach since hashtables in python are unordered.
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
 
